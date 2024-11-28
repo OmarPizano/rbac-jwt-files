f220b55714cb52e987930b64043c3844e21e11a4
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-11-28 13:01:13 -0600

    fix(auth): fix authentication middleware
    Confirm token presence, validity and expected values.

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,12 +1,21 @@
 import jwt from "jsonwebtoken";
 import jwtConfig from "../config/jwt.js";
 
+// authentication
 export const authenticateToken = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token) return res.sendStatus(401);
+  // extract auth header
+  const authHeader = req.headers["authorization"];
+  if (!authHeader || !authHeader.startsWith("Bearer ")) return res.sendStatus(401);
 
+  // extract token from header
+  const token = authHeader.split(" ")[1];
+  
   jwt.verify(token, jwtConfig.secret, (err, decodedPayload) => {
+    // invalid or expired token
     if (err) return res.sendStatus(403);
+    // malformed token
+    if (!decodedPayload.id || !decodedPayload.username || !decodedPayload.role_id) return res.sendStatus(400);
+    // if everything OK, insert accountData
     req.accountData = decodedPayload;
     next();
   });
