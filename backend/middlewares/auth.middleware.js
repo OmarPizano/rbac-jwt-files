bc6e5189dfaf925684c76956764dd612fe2c5b6c
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-11-28 13:41:55 -0600

    refactor(auth): simplify authentication/authorization mechanism

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,30 +1,32 @@
 import jwt from "jsonwebtoken";
 import jwtConfig from "../config/jwt.js";
 
-// authentication
-export const authenticateToken = (req, res, next) => {
+export const auth = (requiredRole) => (req, res, next) => {
+  // 1. authentication
   // extract auth header
   const authHeader = req.headers["authorization"];
   if (!authHeader || !authHeader.startsWith("Bearer ")) return res.sendStatus(401);
 
   // extract token from header
   const token = authHeader.split(" ")[1];
-  
-  jwt.verify(token, jwtConfig.secret, (err, decodedPayload) => {
-    // invalid or expired token
-    if (err) return res.sendStatus(403);
-    // malformed token
-    if (!decodedPayload.id || !decodedPayload.username || !decodedPayload.role_id) return res.sendStatus(400);
-    // if everything OK, insert accountData
-    req.accountData = decodedPayload;
-    next();
-  });
-};
 
-// authorization
-// this middleware always executes after authentication middleware
-export const authorizeRole = (role) => (req, res, next) => {
-  const roles = { "admin": 1, "user": 2 };
-  if (req.accountData.role_id !== roles[role] ) return res.sendStatus(403);
-  next();
+  try {
+    // extract jwt decoded payload
+    const payload = jwt.verify(token, jwtConfig.secret);
+
+    // malformed token
+    if (!payload.id || !payload.username || !payload.role_id) return res.sendStatus(400);
+  
+    // 2. authorization
+    const roles = { "admin": 1, "user": 2 };
+    if (payload.role_id !== roles[requiredRole] ) return res.sendStatus(403);
+
+    // insert accountData for further frontend processing
+    req.accountData = payload;
+
+    next();
+
+  } catch (error) {
+    return res.sendStatus(403);
+  }
 }
--- backend/routes/role.routes.js
+++ backend/routes/role.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import Role from "../models/role.model.js";
+import { auth } from "../middlewares/auth.middleware.js";
 
 const roleRouter = express.Router();
 
 // TODO: check for admin role
-roleRouter.get("/roles", async (req, res) => {
+roleRouter.get("/roles", auth("admin"), async (req, res) => {
   const roles = await Role.findAll();
   res.json(roles);
 })
