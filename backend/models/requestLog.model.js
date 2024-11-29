a77278d9f841f8ec8484e3603c7d2f4a576c5e91
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-11-29 13:56:27 -0600

    feature(request-log): add model for request logging

---
+++ backend/models/requestLog.model.js
@@ -0,0 +1,45 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import Account from "./account.model.js";
+
+const RequestLog = sequelize.define("RequestLog", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  account_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: Account,
+      key: "id",
+    }
+  },
+  method: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  endpoint: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status_code: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  client_ip: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  request_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+  tableName: "request_log",
+});
+
+export default RequestLog;
