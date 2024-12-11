06d6038f3a0491ec2719ea0e812c60f79fa4959f
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-12-11 17:38:31 -0600

    refact(auth): normalize json responses, try-catch

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,22 +7,32 @@ import Account from "../models/account.model.js";
 const authRouter = express.Router();
 
 authRouter.post("/auth", async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  // search account
-  const account = await Account.findOne({ where: { username: username } });
+    // validate params
+    if (!username || !password) return res.status(400).json({ message: "missing username or password" });
 
-  // verify password
-  if (! await bcrypt.compare(password, account.password)) return res.sendStatus(401);
+    // search account
+    const account = await Account.findOne({ where: { username: username } });
+    if (!account) return res.status(401).json({ message: "invalid credentials" });
 
-  // create token
-  // parseInt because: 3600 = '1h', '3600' = 3.6 seconds
-  const token = jwt.sign({
-    id: account.id,
-    username: account.username,
-    role_id: account.role_id,
-  }, jwtConfig.secret, { expiresIn: parseInt(jwtConfig.expiresIn, 10) }); 
-  res.json({ token });
-})
+    // verify password
+    if (! await bcrypt.compare(password, account.password)) return res.status(401).json({ message: "invalid credentials" });
+
+    // create token
+    // NOTE: using parseInt because: 3600 = '1h', '3600' = 3.6 seconds
+    const token = jwt.sign({
+      id: account.id,
+      username: account.username,
+      role_id: account.role_id,
+    }, jwtConfig.secret, { expiresIn: parseInt(jwtConfig.expiresIn, 10) }); 
+
+    res.status(200).json({ data: token });
+  } catch (error) {
+    console.log(`ERROR: authentication: ${error}`);
+    res.status(500).json({ message: "internal server errror" });
+  }
+});
 
 export default authRouter;
