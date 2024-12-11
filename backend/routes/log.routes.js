ea19f231f020cf3d6b22dd913f445c73a24f6186
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-12-11 17:57:30 -0600

    refact(request-logs): normalize json responses, try-catch

--- backend/routes/log.routes.js
+++ backend/routes/log.routes.js
@@ -5,8 +5,13 @@ import { auth } from "../middlewares/auth.middleware.js";
 const logsRouter = express.Router();
 
 logsRouter.get("/logs", auth("admin"), async (req, res) => {
-  const logs = await RequestLog.findAll();
-  res.json(logs);
-})
+  try {  
+    const logs = await RequestLog.findAll();
+    res.status(200).json({ data: logs });
+  } catch (error) { 
+    console.log(`ERROR: ${req.method} ${req.originalUrl}: ${error}`);
+    res.status(500).json({ message: "internal server errror" });
+  }
+});
 
 export default logsRouter;
