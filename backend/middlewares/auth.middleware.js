eee35e0516014638fb0af6a8760ee3fc4c9ea655
OmarPizano <oj.pizanoorozco@ugto.mx>
2024-12-11 19:18:23 -0600

    refact(auth-middleware): normalize json responses, try-catch

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ export const auth = (...allowedRoles) => (req, res, next) => {
   // check auth header
   const authHeader = req.headers["authorization"];
   // if there is no auth header returns Unauthorized
-  if (!authHeader || !authHeader.startsWith("Bearer ")) return res.sendStatus(401);
+  if (!authHeader || !authHeader.startsWith("Bearer ")) return res.status(401).json({ message: "missing authorization token" });
 
   // extract token from header
   const token = authHeader.split(" ")[1];
@@ -21,7 +21,7 @@ export const auth = (...allowedRoles) => (req, res, next) => {
     const payload = jwt.verify(token, jwtConfig.secret);
 
     // if the payload does not contain what is expected, returns Bad Request
-    if (!payload.id || !payload.username || !payload.role_id) return res.sendStatus(400);
+    if (!payload.id || !payload.username || !payload.role_id) return res.status(400).json({ message: "unexpected token content" });
 
     // insert accountData for further frontend processing
     // NOTE: accountData is created BEFORE the authorization check because every response triggers the request logger and, in this case, we need the accountData.id to log it to the DB
@@ -31,16 +31,16 @@ export const auth = (...allowedRoles) => (req, res, next) => {
     // check if authenticated user have one of the allowed roles
     const roleFound = allowedRoles.filter((role) => roles[role] == payload.role_id);
     // if there is no matching role returns Forbidden
-    if (roleFound.length == 0) return res.sendStatus(403);
+    if (roleFound.length == 0) return res.status(403).json({ message: "access denied to requested resource" });
 
     next();
-
   } catch (error) {
     if (error.name === "TokenExpiredError" || error.name === "JsonWebTokenError") {
-      return res.sendStatus(401);
+      return res.status(401).json({ message: "expired or invalid token" });
     }
 
-    // unknown error
-    return res.sendStatus(500);
+    // other error
+    console.log(`ERROR: (auth middleware) ${req.method} ${req.originalUrl}: ${error}`);
+    res.status(500).json({ message: "internal server errror" }); 
   }
 }
